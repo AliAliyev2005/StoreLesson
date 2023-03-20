a873491a84fd3a351009d6b545eb3dcc0d9822aa
Ali <Xliy_g8zy@itstep.edu.az>
2023-03-20 19:35:23 +0400

    add some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Container } from "@chakra-ui/react";
 import { Navbar } from "./components/Navbar.js";
 
 function App() {
-    return <Navbar />;
+    return (
+        <div className="app">
+            <Container maxW="1200px">
+                <Navbar />
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Input, useDisclosure } from "@chakra-ui/react";
+import { Box, Button, Image, Input, useDisclosure, Grid, GridItem, Container, HStack } from "@chakra-ui/react";
 import {
     Drawer,
     DrawerBody,
@@ -15,54 +15,54 @@ const Navbar = () => {
     const btnRef = React.useRef();
 
     return (
-        <Box
-            __css={{
-                width: "100vw",
-                height: "13vh",
-                boxShadow: "-1px 20px 50px -14px",
-                display: "flex",
-                alignContent: "center",
-            }}>
-            <div className="container d-flex justify-content-between">
-                <Image src="logo.png"></Image>
-                <div className="d-flex w-75">
+        <Grid
+            templateAreas={`"logo search"
+                            "category filter"
+                            "category products"`}
+            gridTemplateRows={"75px 75px 1fr"}
+            gridTemplateColumns={"300px 1fr"}
+            h="calc(100vh)"
+            gap="1"
+        >
+            <GridItem area={"logo"}>
+                <Image h={"75px"} fit={"cover"} src="logo.png"></Image>
+            </GridItem>
+            <GridItem area={"search"}>
+                <HStack>
                     <Input placeholder="Search" />
                     <Button className="ms-2" colorScheme="green">
                         Search
                     </Button>
-                    <Button
-                        className="ms-2"
-                        ref={btnRef}
-                        colorScheme="teal"
-                        onClick={onOpen}>
-                        Pay
-                    </Button>
-                    <Drawer
-                        isOpen={isOpen}
-                        placement="right"
-                        onClose={onClose}
-                        finalFocusRef={btnRef}>
-                        <DrawerOverlay />
-                        <DrawerContent>
-                            <DrawerCloseButton />
-                            <DrawerHeader>Review your purchaces</DrawerHeader>
+                </HStack>
+            </GridItem>
+            <GridItem area={"category"} bg="green.300"></GridItem>
+            <GridItem area={"products"} bg="purple.300"></GridItem>
+            <GridItem area={"filter"} bg="blue.300"></GridItem>
+        </Grid>
+        // <Box>
+        //
+        //
+        //
+        //     <Button className="ms-2" ref={btnRef} colorScheme="teal" onClick={onOpen}>
+        //         Pay
+        //     </Button>
+        //     <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
+        //         <DrawerOverlay />
+        //         <DrawerContent>
+        //             <DrawerCloseButton />
+        //             <DrawerHeader>Review your purchaces</DrawerHeader>
 
-                            <DrawerBody></DrawerBody>
+        //             <DrawerBody></DrawerBody>
 
-                            <DrawerFooter>
-                                <Button
-                                    variant="outline"
-                                    mr={3}
-                                    onClick={onClose}>
-                                    Cancel
-                                </Button>
-                                <Button colorScheme="teal">Pay</Button>
-                            </DrawerFooter>
-                        </DrawerContent>
-                    </Drawer>
-                </div>
-            </div>
-        </Box>
+        //             <DrawerFooter>
+        //                 <Button variant="outline" mr={3} onClick={onClose}>
+        //                     Cancel
+        //                 </Button>
+        //                 <Button colorScheme="teal">Pay</Button>
+        //             </DrawerFooter>
+        //         </DrawerContent>
+        //     </Drawer>
+        // </Box>
     );
 };
 
