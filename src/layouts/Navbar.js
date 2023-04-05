5a510d7fab63671ce39b868eb6a6260800117519
Ali <Xliy_g8zy@itstep.edu.az>
2023-04-05 17:10:16 +0400

    change style of product

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,14 +1,4 @@
-import {
-    Box,
-    Button,
-    Container,
-    Flex,
-    Heading,
-    HStack,
-    Input,
-    InputGroup,
-    InputLeftElement,
-} from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Heading, HStack, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useGlobalContext } from "../context/GlobalContext";
 import useBasket from "../pages/Home/hooks/useBasket";
 import { Search2Icon } from "@chakra-ui/icons";
@@ -23,24 +13,14 @@ function Navbar() {
     return (
         <Box h={"60px"} bg="teal.400">
             <Container h={"100%"} maxW={"1200px"}>
-                <Flex
-                    h={"100%"}
-                    justifyContent={"space-between"}
-                    alignItems={"center"}>
+                <Flex h={"100%"} justifyContent={"space-between"} alignItems={"center"}>
                     <Heading color={"white"}>store.az</Heading>
                     <InputGroup maxW={"800px"}>
-                        <InputLeftElement
-                            pointerEvents="none"
-                            color="gray.300"
-                            fontSize="1.2em"
-                            children={<Search2Icon />}
-                        />
+                        <InputLeftElement pointerEvents="none" color="gray.300" fontSize="1.2em" children={<Search2Icon />} />
                         <Input bg={"white"} placeholder="Axtarış" />
                     </InputGroup>
                     <HStack>
-                        <Button
-                            rightIcon={<ImFilter />}
-                            onClick={onToggleFilter}>
+                        <Button rightIcon={<ImFilter />} onClick={onToggleFilter}>
                             Filter
                         </Button>
                         <Button rightIcon={<BsCart3 />} onClick={onOpen}>
--- src/pages/Home/components/Body/Product.js
+++ src/pages/Home/components/Body/Product.js
@@ -6,29 +6,46 @@ function Product({ data }) {
     const { Counter, count, increment } = useProductCounter(data);
 
     return (
-        <Box p="5" maxW="320px" borderWidth="1px" borderRadius={10}>
-            <Image borderRadius="md" src={`https://source.unsplash.com/random/480x270?item`} />
-            <Flex align="baseline" justifyContent={"space-between"} mt={2}>
-                {data.is_vip ? <Badge colorScheme="yellow">VIP</Badge> : <span></span>}
-                <Flex mt={2} align="center">
-                    <Box as={MdStar} color="orange.400" />
-                    <Text ml={1} fontSize="sm">
-                        <b>{data.rating}</b>
-                    </Text>
+        <Box
+            maxW="320px"
+            borderWidth="1px"
+            display={"flex"}
+            flexDir={"column"}
+            position={"relative"}
+            h={"280px"}
+            borderRadius={10}
+            _hover={{ boxShadow: "0 4px 8px 0 rgba(177,181,199,0.48)", cursor: "pointer" }}
+        >
+            <Image
+                borderRadius="md"
+                style={{ borderRadius: "10px 10px 0px 0px" }}
+                src={`https://source.unsplash.com/random/480x270?item`}
+            />
+            <Box p={5} pt={0} h={"100%"} display={"flex"} flexDir={"column"} justifyContent={"space-between"}>
+                <Flex w={"100%"} position={"absolute"} top={0} left={0} px={3} align="baseline" justifyContent={"space-between"}>
+                    {data.is_vip ? <Badge colorScheme="yellow">VIP</Badge> : <span></span>}
+                    <Flex bg={"white"} borderRadius={4} mt={2} px={1} align="center">
+                        <Box as={MdStar} color="orange.400" />
+                        <Text ml={1} fontSize="sm">
+                            <b>{data.rating}</b>
+                        </Text>
+                    </Flex>
                 </Flex>
-            </Flex>
-            <Text mt={2}>{data.name}</Text>
-            <Flex mt={2} alignItems={"center"} justifyContent={"space-between"}>
-                <Text fontSize="2xl" lineHeight="short" fontWeight="semibold">
-                    ${data.price}
+                <Text mt={2} fontSize={"xl"}>
+                    {data.name}
                 </Text>
-                <Flex>
-                    <Button display={count < 1 ? "block" : "none"} colorScheme={"teal"} size={"sm"} onClick={increment}>
-                        Səbətə əlavə et
-                    </Button>
-                    {Counter}
+                <Flex mt={2} alignItems={"center"} justifyContent={"space-between"}>
+                    <Text fontSize="2xl" lineHeight="short" fontWeight="semibold">
+                        ${data.price}
+                    </Text>
+                    <Flex>
+                        <Button display={count < 1 ? "block" : "none"} colorScheme={"teal"} size={"sm"} onClick={increment}>
+                            Səbətə əlavə et
+                        </Button>
+                        {Counter}
+                    </Flex>
                 </Flex>
-            </Flex>
+            </Box>
         </Box>
     );
 }
